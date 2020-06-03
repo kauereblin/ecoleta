78443f4669210873b821f4c1614f2ede6d8f9379
kauereblin <kaue.reblin159@gmail.com>
2020-06-03 00:50:07 -0300

    Update backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,18 @@
 import express from "express";
 
-const routes = express.Router();
+import PointsController from "../src/controllers/PointsController";
+import ItemsController from "../src/controllers/ItemsController";
 
-routes.get('/', (req, res) => {
-  return res.json({ message: "foda" })
-})
+const routes = express.Router();
+const pointsController = new PointsController;
+const itemsController = new ItemsController;
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+// index, show, create, update, delete
 
 export default routes;
